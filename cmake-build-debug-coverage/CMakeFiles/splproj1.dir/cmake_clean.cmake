367f4293c583da96604de1986b01b6b85e54b030
yarden1032 <yarden1032@gmail.com>
2020-11-04 08:26:41 +0200

    changing Session.cpp notice it's not full just making it work in running time

---
+++ cmake-build-debug-coverage/CMakeFiles/splproj1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/splproj1.dir/src/main.cpp.o"
+  "splproj1.pdb"
+  "splproj1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/splproj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
