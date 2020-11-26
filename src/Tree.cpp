7f0ed116958c83760400760dbe5908bb882a8b8a
yarden1032 <yarden1032@gmail.com>
2020-11-26 14:46:36 +0200

    Finished - final stuff

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -9,7 +9,6 @@
 #include "../include/Session.h"
 #include <iostream>
 #include <vector>
-#include "../include/Agent.h"
 using namespace std;
 
 Tree::Tree() {
@@ -20,20 +19,15 @@ Tree::Tree(int rootLabel)
     {
     node=rootLabel;
     }
-
+/*Tree::~Tree() {
+    clear();
+}*/
 Tree::~Tree() {
     clear();
 }
-Tree* Tree::createTree(const Session& session, int rootLabel)
-{
-    Session& session1 = const_cast< Session&>(session);
-    return ContactTracer::BFS(rootLabel, session1);
-
-   // return BFS(rootLabel, session);
-}
 void Tree::addChild(const Tree& child)
 {
-
+ //   Tree * tempChild = const_cast<Tree *>(&child); //TODO:tocheck
 children.push_back(const_cast<Tree *>(&child));
 //delete tempChild;
 }
@@ -51,6 +45,12 @@ int Tree::getNode() {
 }
 void Tree::clear() {
     node=0;
+    /*for(int i=0 ; i<children.size();i++) {
+        children[i]->clear();
+        children[i]= nullptr;
+
+    }
+    children.clear();*/
 
     for (auto p : children)
     {
@@ -112,9 +112,53 @@ MaxRankTree::~MaxRankTree() {
  */
 
      int MaxRankTree::traceTree(){
+/*
+         int max=0;
+         vector<int> maxint;
+         int i;
+         for (i=0;i<this->getChildren().size();i++)
+    {
+        if (this->getChildren()[i]->getChildren().size()>max)
+        {
+            max=getChildren()[i]->getChildren().size();
+            maxint.clear();
+           maxint.push_back(i);
+        }
+        else {
+            if (this->getChildren()[i]->getChildren().size() == max) {
+                maxint.push_back(i);
+            }
+        }
 
+    }
+    if(maxint.size()==1)
+    {
+       // int max= ;
+
+     //   delete this->[max];    //TODO: think about it we have issue here for now
+        return this->getChildren()[(maxint[0])]->getNode();
+    }if (maxint.size()==0)
+         {
+        return getNode();
+         }
+
+    else
+    {
+    //    int max=;
+int iti = minDepthHelper(this, maxint);
+        return getChildren()[iti]->getNode();
+    }*/
+/**
+ * for max tree we need to searrch the node with the most children - easy - we can do iteration to all of nodes
+ *
+ * if we find 2 with the same size - return them as vector
+ *          we will check from them who is the closest one to the root - iteration?
+ *
+ *
+ */
     int  max=-1;
     vector<int> maxint;
+    MaxRankTree * temp=this;
     maxint = traceTreeIteration(this, maxint,max);
     if (maxint.size()==1) {
         return maxint[0];
@@ -162,7 +206,7 @@ MaxRankTree::~MaxRankTree() {
 
 int MaxRankTree::traceTree_Leftest(Tree * node, vector<int> maxint,  vector<vector<int>> trip_maxint) {
         bool b=true; int currnode=node->getNode();
-         int step=-1; 
+         int step=-1; int safetogo=0;
          while(b)   {
 
         for (int i=0;i<trip_maxint.size();i++)
@@ -253,12 +297,173 @@ std::vector<int> MaxRankTree::traceTreeIteration(Tree* node,std::vector<int> & m
 
 }
 
+//TODO fix here - important for config4.JSON
+      /*  int MaxRankTree::minDepthHelper(Tree* node, vector<int> maxint,  vector<int> * minintdepth) {
+    if (node == NULL)
+        return 0;
+    else {
+        ///start
+        int i;
+    //    vector<int> minintdepth = *new vector<int>(node->getChildren().size());
+      //  int maxint = -1;
+        for (i = 0; i < node->getChildren().size(); i++) {
+            //call to itself with the children
+
+            (*minintdepth)[i] = minDepthHelper(node->getChildren()[i], maxint,minintdepth) + 1;
+        }
+        int j = -1;
+        int min=-5;
+        for (j=0; j < minintdepth->size(); j++) {
+            if (min < (*minintdepth)[j]||min<0) {
+                min =   (*minintdepth)[j];
+
+            }
+        }
+        for(int k=0;k< node->getChildren().size(); k++) {
+            if (min == (*minintdepth)[k])
+            {
+                delete node->getChildren()[k];
+                return  k;
+
+            }
+
+
+        }
+        return 0;
+
+
+    }
+
+}
+
+*/
+/*here we wrote how to get depth in recursion - it works for sure
+ *
+ */
+/*
+     int  MaxRankTree::minDepthHelperIteration(Tree* node) {
+
+             if (node->getChildren().size() == 0)
+                 return 1;
+             else
+             {
+                 // compute the depth of each subtree
+                 vector<int> vecy;
+                 for (int i = 0; i < node->getChildren().size(); i++) {
+                     vecy.push_back(minDepthHelperIteration(node->getChildren()[i]));
+
+                 }
+
+          //       int lDepth = maxDepth(node->left);
+       //          int rDepth = maxDepth(node->right);
+
+                 /// use the larger one
+                 int max=-1;
+          for (int i = 0; i < node->getChildren().size(); i++)
+          {
+              if (max<=vecy[i])
+              {
+                  max=vecy[i];
+              }
+          }
+          return max;
+             }
+      }
+
+
+/**
+ * we use here the iteration above
+ * @param node
+ * @param maxint
+ * @return min depth
+ */
+
+
+/*
+
+         int  MaxRankTree::minDepthHelper(Tree* node, vector<int> maxint) { //we return the index
+
+    vector<int>  vecy; //vector of all the maxint's depth - we will use the iteration to do so
+   int min =-5; int minpos=-1;
+    int counterchecker=0;
+
+    for (int j = 0; j < maxint.size(); j++) {
+
+    for (int i = 0; i < node->getChildren().size(); i++){
+
+        if (i==maxint[j]) {
+            vecy.push_back(minDepthHelperIteration(node->getChildren()[i]));
+            break;
+        }
+                     }
+
+                 }
+
+
+    for (int j = 0; j < maxint.size(); j++){
+   if (vecy[j]==min)
+   {
+       counterchecker++;
+   }
+
+        if (min==-5 ||vecy[j]<min)
+        {
+            min=vecy[j];
+            counterchecker=1;
+            minpos=j;
+        }
+             }
+
+    if (counterchecker>1)
+    {
+        return 0;
+    }
+
+    return minpos;
+         }
+*/
+/*
+    int maxDepth(Tree* node)
+
+    {
+        for (i=0;i<node->getChildren().size();i++) {
+
+            if (node->getChildren()[i]->getChildren().size()>max)
+            {
+                max=getChildren()[i]->getChildren().size();
+                maxintdepth.clear();
+                maxintdepth.push_back(i); //call to itself
+            }
+            if (node->getChildren()[i]->getChildren().size()==max)
+            {
+                maxintdepth.push_back(i); //call to itself
+            }
+
+
+        }
+
+        ////end
+        /* compute the depth of each subtree
+        int lDepth = maxDepth(node->left);
+        int rDepth = maxDepth(node->right);
+
+        /* use the larger one
+        if (lDepth > rDepth)
+            return(lDepth + 1);
+        else return(rDepth + 1);
+    }
+}
+*/
+
+
+
+
 
 
 void CycleTree::setCurrCycle(int currCycle) {
     this->currCycle = currCycle;
 }
-int CycleTree::getCurrCycle() {
+int CycleTree::getCurrCycle() const {
     return currCycle;
 }
 
@@ -294,10 +499,39 @@ int CycleTree::traceTree() {
     int n = this->getCurrCycle();
  if(cycleTrip.size()>n+1){
 
-    return cycleTrip[n];
+    return cycleTrip[n+1];
  }
  return cycleTrip[cycleTrip.size()-1];
 
+/*
+    int CurrCycle=this->getCurrCycle();
+
+    int lastindex = cycleTrip[cycleTrip.size()-1];
+
+    if (cycleTrip.size()<CurrCycle) {
+      /*  for (int i = 0; i < this->getChildren().size(); i++) {
+            delete this->getChildren()[lastindex];
+        }
+        return (cycleTrip[lastindex]);
+
+    }
+   /* for (int i = 0; i < this->getChildren().size(); i++) {
+        delete this->getChildren()[CurrCycle];
+    }
+    return cycleTrip[CurrCycle];
+
+*/
+
+
+
+    /**
+     * we choose the next node by the type of the tree
+     *
+     * than we delete all the connected edges of the current one
+     *
+     * return the chosen one
+     *
+     */
 
 }
 
